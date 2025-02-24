460ee95e50f49c3d7ed2ba0c9b29a5c3e7797696
Lucas Hamad-Chetan <hani.lucas.lhc@gmail.com>
2025-02-24 15:01:14 +0100

    Initialising and setting up the Vue project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parserOptions: {
+        requireConfigFile: false,
+        parser: "@babel/eslint-parser"
+    },
+    extends: [
+        "plugin:vue/vue3-recommended",
+        "eslint:recommended"
+    ]
+};
